71173bd175a446cfb2535fed666576f553f83f13
cpaschall <cpaschall216@gmail.com>
2023-08-08 19:26:54 -0400

    initial React components

---
+++ client/TODO.md
@@ -0,0 +1,2 @@
+# TODO 1 
+- [x] When I click on "Add Section" and new section appears in alphabetical order in section navbar
---
+++ client/src/components/SectionNav.js
@@ -0,0 +1,12 @@
+export default function SectionNav({ currentSection, handlePageChange }) {
+    return (
+        <div id="section-nav">
+            <h3>Section Nav</h3>
+            <ul id="section-links">
+                <li>
+                    
+                </li>
+            </ul>
+        </div>
+    )
+}
